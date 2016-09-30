b8b01891c3ebff8bcc1911f6c19a9e402f4c5600
Tai @ Flex <dch.tai@gmail.com>
2016-09-30 17:39:54 +0100

    new builds

--- bin/install.sh
+++ bin/install.sh
@@ -72,6 +72,116 @@ function dumpe {
 if [[ "$*" =~ --debug ]]; then
 	MODE_DEBUG=yes
 fi
+#!/bin/bash
+
+### AskUser Usage:bbuild
+# Present the user with questions on stdout
+###/doc
+
+
+yespat='^(yes|YES|y|Y)$'
+numpat='^[0-9]+$'
+rangepat='[0-9]+,[0-9]+'
+blankpat='^ *$'
+
+### uconfirm Usage:bbuild
+# Ask the user to confirm a closed question. Defaults to no
+#
+# returns 0 on successfully match 'y' or 'yes'
+# returns 1 otherwise
+###/doc
+function uconfirm {
+	read -p "$* [y/N] > " 1>&2
+	if [[ "$REPLY" =~ $yespat ]]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+### uask Usage:bbuild
+# Ask the user to provide some text
+#
+# returns the entered text
+###
+function uask {
+	read -p "$* : " 1>&2
+	echo $REPLY
+}
+
+### uchoose_multi Usage:bbuild
+# Allows the user to choose from multiple choices
+#
+# uchose_multi MESG CHOICESTRING
+#
+#
+# MESG is a single string token that will be displayed as prompt
+#
+# CHOICESTRING is a comma-separated, or newline separated, or "\\n"-separated token string
+#
+# Equivalent strings include:
+#
+# * `"a\\nb\\nc"` - quoted and explicit newline escapes
+# * `"a,b,c"` - quoted and separated with commas
+# * `a , b , c` - not quoted, separated by commas
+# * (and `a`, `b` and `c` on their own lines)
+#
+# User input:
+#
+# User can choose by selecting
+#
+# * a single item by number
+# * a range of numbers (4,7 for range 4 to 7)
+# * or a string that matches the pattern
+#
+# All option lines that match will be returned, one per line
+#
+# If the user selects nothing, then function returns 1 and an empty stdout
+###/doc
+function uchoose_multi {
+	local mesg=$1; shift
+	local choices=$(echo "$*"|sed -r 's/ *, */\n/g')
+	debuge "CHOICES: $choices"
+
+	infoe "$mesg:" 
+	echo -e "$choices"|egrep '^' -n| sed 's/:/: /' 1>&2
+	
+	local sel=$(uask "Choice")
+	if [[ "$sel" =~ $blankpat ]]; then
+		return 1
+	elif [[ "$sel" =~ $numpat ]] || [[ "$sel" =~ $rangepat ]]; then
+		debuge "Number choice [$sel]"
+		echo -e "$choices" | sed -n "$sel p"
+	else
+		debuge "Pattern choice [$sel]"
+		echo -e "$choices"  |egrep "$(echo "$sel"|tr " " '|')"
+	fi
+	return 0
+}
+
+### uchoose Usage:bbuild
+# Ask the user to choose an item
+#
+# Like uchoose_multi, but will loop if the user selects more than one item
+#
+# If the provides no entry, returns 0
+#
+# If the user chooses one item, that item is echoed to stdout
+###/doc
+function uchoose {
+	local mesg=$1; shift
+	while true; do
+		local thechoice="$(uchoose_multi "$mesg" "$*")"
+		local lines=$(echo "$thechoice" | wc -l)
+		if [[ $lines = 1 ]]; then
+			echo "$thechoice"
+			return 0
+		else
+			warne "Too many results"
+		fi
+	done
+}
+
 
 if [[ "$UID" -gt 0 ]]; then faile You must be root to run this command
 fi
@@ -82,9 +192,19 @@ apt-get update && apt-get install git anacron -y
 
 cd
 
-git clone https://github.com/taikedz/partimus-updater
+if [[ ! -d partimus-updater ]]; then
+	git clone https://github.com/taikedz/partimus-updater
+fi
 
 cd partimus-updater
 cp bin/cronjob /etc/cron.daily/
+
+cp partimus.conf /etc/
+
+emails=$(askuser "Email(s) to send notifications to")
+sed "s/EMAIL=/EMAIL=$emails/" -i /etc/partimus.conf
+
+machinename=$(askuser "Name of this machine (for the emails)")
+sed "s/MACHINE=/MACHINE=$machinename/" -i /etc/partimus.conf
 
 infoe Completed successfully.
