6bcd1f599f027525cd0187a11b88fee2510fcf43
Tai @ Flex <dch.tai@gmail.com>
2016-09-30 17:16:26 +0100

    add configuration questions

--- src/install.sh
+++ src/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#%include bashout
+#%include bashout askuser
 
 if [[ "$UID" -gt 0 ]]; then faile You must be root to run this command
 fi
@@ -11,9 +11,19 @@ apt-get update && apt-get install git anacron -y
 
 cd
 
-git clone https://github.com/taikedz/partimus-updater
+if [[ ! -d partimus-updater ]]; then
+	git clone https://github.com/taikedz/partimus-updater
+fi
 
 cd partimus-updater
 cp bin/cronjob /etc/cron.daily/
+
+cp partimus.conf /etc/
+
+emails=$(askuser "Email(s) to send notifications to")
+sed "s/EMAIL=/EMAIL=$emails/" -i /etc/partimus.conf
+
+machinename=$(askuser "Name of this machine (for the emails)")
+sed "s/MACHINE=/MACHINE=$machinename/" -i /etc/partimus.conf
 
 infoe Completed successfully.
